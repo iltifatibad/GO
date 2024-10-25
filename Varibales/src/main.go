e0b0a9576fc217b23585ffb0e432e10187d31fc5
iltifatibad <iltifatibad@gmail.com>
2024-10-26 01:11:08 +0400

    Variable

---
+++ Varibales/src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = " Hello "
+
+	var firstname, lastname string = " Hello ", "World "
+
+	name2 := " Hello World " // It gives auto Data Type For Example It's String
+
+	fmt.Println(name, " World ")
+	fmt.Println(firstname, lastname)
+	fmt.Println(name2)
+}
